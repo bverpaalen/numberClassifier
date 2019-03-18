f956c0246d79b4d7efc11613f3e3b3c0bafbdd55
Unmukt <unmukt.deswal@gmail.com>
2019-03-18 15:20:50 +0100

    Add files via upload

--- task4.py
+++ task4.py
@@ -1,10 +1,12 @@
 import numpy as np
+import pandas as pd
 
-trainDataPath = "./data/train_"
+
+trainDataPath = "D:/Career/Leiden/Masters/2nd Semester/Neural Networks/Assignments/Assignment 1/numberClassifier/data/train_"
 trainDataIn = trainDataPath + "in.csv"
 trainDataOut = trainDataPath + "out.csv"
 
-testDataPath = "./data/test_"
+testDataPath = "D:/Career/Leiden/Masters/2nd Semester/Neural Networks/Assignments/Assignment 1/numberClassifier/data/test_"
 testDataIn = testDataPath + "in.csv"
 testDataOut = testDataPath + "out.csv"
 
@@ -14,74 +16,50 @@ outputTraining = np.loadtxt(trainDataOut, dtype = 'str', delimiter = ',')
 inputTest = np.loadtxt(testDataIn, dtype = 'str', delimiter = ',')
 outputTest = np.loadtxt(testDataOut, dtype = 'str', delimiter = ',')
 
-class perceptron(object):
+class Perceptron:
     
-    def __init__(self, inputData, outputData, learning = 0.1):
-        self.inputData = self.add_bias(inputData)
+    #Contructor for initializing the object:
+    def __init__(self, inputData, outputData, learning = 0.1, weights):
+        self.inputData = addBias(inputData)
         self.outputData = outputData
         self.learning = learning
+        self.weights = initialWeights(inputData)
         
-        self.weights = self.addWeights(inputTraining, outputTraining, learning)
-        self.accuracy = self.accuracy(inputTest, outputTest, self.weights)
-        
-    def addBias(inputData):
+    def addBias(self, inputData):
         a, b = np.shape(inputData)
         c = np.ones((a, 1))
         return np.hstack(c, inputData)
-        
+    
     def initialWeights(self, inputData):
         a, b = np.shape(inputData)
-        weights = np.ramdom.rand(b, 1)
+        
+        for i in range(0, 10):
+            weights[:, i] = np.ramdom.rand(b, 1)
+        
         return weights
     
-    def predictInitial(self, node, weights):
-        activation = np.dot(node, weights)
-        a = activation>0
-        return a*1
     
-    def addWeights(self, inputData, outputData, learning):
-        c,d = np.shape(inputData)
-        w = self.initialWeights(inputData)
-        weights = []
-        for i in range(0,  len(np.unique(outputData))):
-            if (outputData == i):
-                z = 1
-            else:
-                z = 0
+    #The Perceptron Algorithm for computing the output of the perceptron
+    #with inputData as the activation for each input node of the perceptron:
+    def feedForward(self, inputData, outputData, weights):
+        a, b = np.shape(inputData)
+        
+        for i in range(0, len(a)):
+            weightedSum = np.dot(weights, inputData[i,:])
+            x, predictedOutput = np.shape(np.argmax(weightedSum)) #Determines the image class in predictedOutput
+            bestActivation = np.argmax(weightedSum)
+        
+        return predictedOutput, bestActivation
+
+    
+    #Evaluating the class of the image and update weights accordingly:
+    def backPropagation(self, outputData, predictedOutput):
+       
+        for i in range(0, len(outputData)):
             
-            a = self.training(inputData, z, w, learning)
-            weights.append(a[:,0])
-        return np.asarray(weights)
-    
-    def updateWeights(self, inputData, weights):
-        a = np.dot(inputData,np.transpose(weights))
-        b = len(np.shape(inputData))
-        if b == 1:
-            return np.argmax(a)
-        return np.argmax(a, axis=1)
-    
-    def accuracy(self, inputTest, outputTest, weights):
-        x, y = np.shape(outputTest)
-        predicted = self.updateWeights(inputTest, weights)
-        correct = predicted == outputTest[:,0]
-        accuracy = np.sum(correct)/float(x)
-        return accuracy
-    
-    def reclassify(self, weights, node, output, learning = 0.1):
-        if (np.dot(node, weights) > 0):
-            predicted = 1
-        else:
-            predicted = 0
-        temp = np.zeros(np.shape(weights))
-        temp[:,0] = learning * np.square(output - predicted) * node
-        return temp + weights
-    
-    def sigmoid(x):
-        return 1 / (1 + np.math.e ** -x)
-    
-    def training(self, inputData, output, weights, learning):
-        
-        for i in range(0, len(inputData)):
-            weights = self.reclassify(weights, inputData[i], output[i], learning)
-        
-        return weights
+            if outputData[i] == predictedOutput[i]:
+                prediction = 1
+                else:
+                    prediction = 0
+            
+            
