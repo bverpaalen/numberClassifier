c80decfa0414c2c3b4e4052d6a51cfe161ce8ab5
Unmukt <unmukt.deswal@gmail.com>
2019-03-20 10:13:06 +0100

    Created Train object for Perceptron class. Needs updateWeights()

--- task4.py
+++ task4.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 
 trainDataPath = "./data/train_"
@@ -10,67 +9,74 @@ testDataPath = "./data/test_"
 testDataIn = testDataPath + "in.csv"
 testDataOut = testDataPath + "out.csv"
 
-inputTraining = np.loadtxt(trainDataIn, dtype = 'str', delimiter = ',')
-outputTraining = np.loadtxt(trainDataOut, dtype = 'str', delimiter = ',')
+inputTraining = np.loadtxt(trainDataIn, delimiter = ',')
+outputTraining = np.loadtxt(trainDataOut, delimiter = ',')
 
-inputTest = np.loadtxt(testDataIn, dtype = 'str', delimiter = ',')
-outputTest = np.loadtxt(testDataOut, dtype = 'str', delimiter = ',')
+inputTest = np.loadtxt(testDataIn, delimiter = ',')
+outputTest = np.loadtxt(testDataOut, delimiter = ',')
 
-class Perceptron:
+
+class Perceptron():
     
     #Contructor for initializing the object:
-    def __init__(self, inputData, outputData, learning = 0.1, weights):
-        self.inputData = addBias(inputData)
+    def __init__(self, inputData, outputData, weights):
+        self.inputData = self.addBias(inputData)
         self.outputData = outputData
-        self.learning = learning
-        self.weights = initialWeights(inputData)
+        self.weights = self.initialWeights(inputData)
         
     def addBias(self, inputData):
         a, b = np.shape(inputData)
         c = np.ones((a, 1))
-        return np.hstack(c, inputData)
+        return np.hstack((c, inputData))
     
-    def initialWeights(self, inputData):
-        a, b = np.shape(inputData)
-        
-        for i in range(0, 10):
-            weights[:, i] = np.ramdom.rand(b, 1)
-        
-        return np.asarray(weights)
+    def initialWeights(self, dim, classes):
+        weights = np.random.rand(dim, classes)
+        return np.array(weights)
     
     
     #The Perceptron Algorithm for computing the output of the perceptron
     #with inputData as the activation for each input node of the perceptron:
     def feedForward(self, inputData, outputData, weights):
         a, b = np.shape(inputData)
+        weightedSum = np.zeros(257, 10)
+        predictedOutput = np.zeros(257, 10)
         
         for i in range(0, len(a)):
-            weightedSum[i,:] = np.dot(weights, inputData[i,:])
-            activations[i,:] = sigmoid(weightedSum)
-            x, predictedOutput = np.shape(np.argmax(activations)) #Determines the image class in predictedOutput
+            weightedSum[i,:] = np.dot(inputData[i,:], np.transpose(weights))
+            predictedOutput[i,:] = self.sigmoid(weightedSum)
         
-        return predictedOutput, activations
+        return predictedOutput
 
     def sigmoid(x):
         return 1 / (1 + np.math.e ** -x)
     
     
-    #Using backpropagation algorithm to update weights accordingly:
-    def backPropagation(self, outputData, predictedOutput, activations, weights):
+    def updateWeight(weights):
         
-        for j in range(0, len(outputData))):
-            
-            for i in range(0, 10):
-                
-                if i == outputData[j]:
-                    desiredActivations[j, i] = 1
-                else:
-                    desiredActivations[j, i] = 0
-            
-        for j in range(0, len(outputData)):
-            
-            cost[j, :] = np.square(activations[j, :] - desiredActivations[j, :])
-            for i in range(0, 10):
-                
-                if cost[j, i] > 0:
-                    weights = 
+
+#Training on 1707 images in the training set of the MNIST data:
+Train = Perceptron()
+
+iTrain = Train.inputData
+oTrain = Train.outputData
+
+#Initialize weights:
+dimensions = 256 + 1 #the dimensions of a 16*16 sized image & 1 as a bias for the perceptron
+classes = 10
+weight_matrix = Train.weights
+
+prediction = Train.feedForward(iTrain, oTrain, weight_matrix)
+count = 1
+misclassify = 0
+
+while(count > 0 or iterations > 1000):
+    count = 0
+    for i in range(0, len(oTrain)):
+        if oTrain == prediction:
+            continue
+        weight_matrix[i] = Train.updateWeight(weight_matrix[i])
+        count += 1
+        misclassify += 1
+    
+    iterations += 1
+    print('Number of misclassified cases in 1000 iterations:' + misclassify)
