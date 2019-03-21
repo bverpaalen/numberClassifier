cb249a204780f5f559901f92330538fba41f612e
bverpaalen <brentverpaalen95@hotmail.com>
2019-03-21 23:16:10 +0100

    task5.1

--- task5.1.py
+++ task5.1.py
@@ -2,12 +2,25 @@ import numpy as np
 import math as m
 import copy
 
-epochs = 100
+epochs = 1000
 epsilon = 0.005
 lr = 0.05
 
 inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
-targets = [0, 1, 1, 0]
+labels = [0, 1, 1, 0]
+
+
+def grdmse(data,network):
+    scoreMatrix = np.empty((len(network.weights),len(network.weights[0])))
+    MSE = network.mse(data,labels)
+    for i in range(len(network.weights)):
+        for j in range(len(network.weights[i])):
+            tNetwork = copy.deepcopy(network)
+            tNetwork.weights[i][j] += epsilon
+            TMSE = tNetwork.mse(data,labels)
+            score = (TMSE-MSE)/epsilon
+            scoreMatrix[i][j] = score
+    return scoreMatrix
 
 class network():
 
@@ -15,19 +28,19 @@ class network():
         trueWidth = max(depth,width)+1
         self.inputNodes = np.empty(inputSize+1)
         self.inputNodes[0] = 1
-        self.hiddenNodes = np.empty(depth,width+1)
+        self.hiddenNodes = np.empty((depth,width+1))
         self.hiddenNodes[:][0] = 1
         self.initialWeights(depth+1,trueWidth)
 
     def initialWeights(self,depth,width):
-        np.random.seed(200)
+        #np.random.seed(329)
         self.weights = np.random.rand(depth, width)
 
     def predict(self,input):
         for i in range(2):
             self.inputNodes[i+1] = input[i]
         self.calculateHiddenNodes()
-        self.calculateOutputNode()
+        return self.calculateOutputNode()
 
     def calculateHiddenNodes(self):
 
@@ -50,32 +63,46 @@ class network():
         for i in range(len(self.hiddenNodes[-1])):
             outputNode += self.weights[-1][i]
         outputNode = outputNode/len(self.hiddenNodes[-1])
+        print(outputNode)
 
         return outputNode
 
-    def mse(self,data):
+    def mse(self,inputs,labels):
         SE = 0
-        for i in range(len(data)):
-            prediction = data[i][0]
-            target = data[i][1]
+        for i in range(len(inputs)):
+            prediction = self.predict(inputs[i])
+            target = labels[i]
 
             SE += (prediction - target)**2
-        MSE = SE/range(data)
+        MSE = np.mean(SE)
+        print(MSE)
         return MSE
 
-    def grdmse(self,data):
-        mse = self.mse(data)
-        #GRDMSE function
+    def changeWeights(self,scoreMatrix):
+        for i in range(len(self.weights)):
+            for j in range(len(self.weights[i])):
+                self.weights[i][j] = self.weights[i][j] - lr*scoreMatrix[i][j]
 
-    def changeWeights(self):
-        #WEIGHT change function
-
+    def test(self,inputs,labels):
+        mismatch = 0
+        for i in range(len(inputs)):
+            prediction = self.predict(inputs[i])
+            if(prediction != labels[i]):
+                mismatch +=1
+        print(mismatch)
 net = network(2,2,2)
 
 for epoch in range(epochs):
     for i in range(len(inputs)):
-        label = targets[i]
-        prediction = net.predict(inputs[i])
+        scoreMatrix = grdmse(inputs,net)
+        net.changeWeights(scoreMatrix)
+
+net.test(inputs, labels)
+
+
+
+
+
 
 
 
